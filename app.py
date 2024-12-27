2366cecd5ddff695fb27fa107d6bf70399ef7ff7
hubbub-inc <matthewcarrella9@gmail.com>
2024-12-26 22:27:57 -0500

    update routes

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import Flask
 app = Flask(__name__)
 
+
+messages = ['first']
+
+@app.route('/test', methods=['POST'])
+def test():
+    values = request.get_json()
+    message = values["message"]
+    messages.append(message)
+    response_message = {'response': messages}
+    return jsonify(response_message)
+
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
